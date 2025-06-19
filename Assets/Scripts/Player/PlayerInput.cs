9e6da232bbd950d08cfc7188cdab7648bf65cbae
SpaghettiTheFlying <149588371+SpaghettiTheFlying@users.noreply.github.com>
2025-06-19 17:10:02 +0300

    Final Commit

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -1,6 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerInput
+[RequireComponent(typeof(PlayerMover))]
+public class PlayerInput : MonoBehaviour
 {
-    
+    private PlayerInputActions inputActions;
+    private PlayerMover mover;
+
+    void Awake()
+    {
+        inputActions = new PlayerInputActions();
+        mover = GetComponent<PlayerMover>();
+    }
+
+    void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    void Update()
+    {
+        Vector2 moveInput = inputActions.Player.Move.ReadValue<Vector2>();
+        mover.Move(moveInput);
+    }
 }
